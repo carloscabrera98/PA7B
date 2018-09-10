f091bd7e60dcaf008c9322e9e9d781d34b53993a
carloscabrera98 <carlosalberto.cabrera@alumno.uttehuacan.edu.mx>
2018-09-10 18:04:13 -0500

    10th september

--- ComputeBMI.java
+++ ComputeBMI.java
@@ -4,6 +4,7 @@ public static void main(String[] args) {
   Scanner input = new Scanner(System.in);
   final double KILOGRAMS_PER_POUND = 0.4535923;
   final double METER_PER_INCH = 0.0254;
+  double BMI;
 
 System.out.println("Enter weight in pounds: ");
 double weight = input.nextDouble();
@@ -13,8 +14,9 @@ double height = input.nextDouble();
 double weighInKilograms = weight * KILOGRAMS_PER_POUND;
 double heightInMeters = height * METER_PER_INCH ;
 
+BMI = (weighInKilograms/ (heightInMeters*heightInMeters));
 
-
+System.out.println("Your IBM is: " + BMI);
 
   }
 }
