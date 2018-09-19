74c084f65088b1e1fe81bfdda0072e023281bc5f
carloscabrera98 <carlosalberto.cabrera@alumno.uttehuacan.edu.mx>
2018-09-19 15:52:59 -0500

    Homework2

---
+++ Homework2/AlgoritmoAlmacenes.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class AlgoritmoAlmacenes{
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    double DE;
+
+
+    System.out.println("Enter a cost of suit: ");
+    int CT= input.nextInt();
+
+
+    if (  CT >= 2500)
+      DE= CT * .15;
+
+    else
+        DE= CT * .08;
+
+double PF= CT - DE;
+System.out.println("Your discount is : " + DE);
+System.out.println("The new cost of suit is : " + PF);
+     }
+   }
