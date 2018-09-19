125777c6152a71309cc68320381b1b06cb117a8e
carloscabrera98 <carlosalberto.cabrera@alumno.uttehuacan.edu.mx>
2018-09-19 15:19:21 -0500

    Homework2

--- Homework2/NumeroPositivo.java
+++ Homework2/NumeroPositivo.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class NumeroPositivo{
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    
+
 
     System.out.println("Enter a  first number: ");
     int NUM= input.nextInt();
@@ -11,7 +11,7 @@ public class NumeroPositivo{
 
 
     if (  NUM > 0) {
-        System.out.println("Is the Postive Number");
+        System.out.println("The number is positive");
     }
     else
         System.out.println("the number is negative");
