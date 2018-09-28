cd2554d7c97fdcea41a29f4860602c475ce1fa2e
carloscabrera98 <carlosalberto.cabrera@alumno.uttehuacan.edu.mx>
2018-09-28 17:06:08 -0500

    27th september

--- DoWhileLoop2.java
+++ DoWhileLoop2.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 public class DoWhileLoop2{
 public static void main(String[] args) {
 int i;
-
+int pregunta= 0;
 do{
   i= Integer.parseInt(JOptionPane.showInputDialog(null, "Escribe el numero de la tabla que deseas realizar"));
   String salida="";
@@ -12,8 +12,9 @@ do{
 
 }
 JOptionPane.showMessageDialog(null, salida);
+pregunta= Integer.parseInt(JOptionPane.showInputDialog(null, "Si no desea otra tabla oprima 0, si desea otra tabla oprima cualquiera"));
 
-}while(i != 0);
-JOptionPane.showMessageDialog(null, "fin");
+}while(pregunta != 0);
+
   }
   }
