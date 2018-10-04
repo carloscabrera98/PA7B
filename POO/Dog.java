57959c646d94ffe1b415f0970b8e7ec862e4ee29
carloscabrera98 <carlosalberto.cabrera@alumno.uttehuacan.edu.mx>
2018-10-04 16:11:09 -0500

    2th october

---
+++ POO/Dog.java
@@ -0,0 +1,30 @@
+public class Dog{
+  //Properties or atributes
+public String color;
+public String eyeColor;
+public Double height;
+public Double length;
+public Double weight;
+
+//Methods or behaviors
+public String sit(){
+  return "Estoy sentado";
+  //System.out.println("Estoy sentado");
+}
+
+public void bark(){
+ System.out.println("wuaff wuaff wuaff");
+}
+
+public void layDown(String name){
+ System.out.println(name + "Estoy recostado");
+}
+
+public void eat(){
+System.out.println("I am eating");
+}
+
+public void sleep(){
+System.out.println("zzzzzzZZZZZ");
+ }
+}
